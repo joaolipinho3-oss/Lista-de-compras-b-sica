a7e7cea91ecec73c168d6da83f68291b8a796193
joaolipinho3-oss <joaolipinho3@gmail.com>
2026-06-29 14:37:12 -0300

    modified:   index.html 	new file:   script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+const sendButton = document.getElementById("botão-enviar")
+const textInput = document.getElementById("input-text")
+
+let preview = document.getElementById("output-produtos")
+
+let adicionarIten = () => {
+    textInput.values
+}
+
+//Eventos de click
+addEventListener('click')
