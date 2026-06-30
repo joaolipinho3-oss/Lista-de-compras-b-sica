95f6e0076e386204dff9e81709a93de0c0bc16bc
joaolipinho3-oss <joaolipinho3@gmail.com>
2026-06-29 22:47:57 -0300

    modified:   index.html 	new file:   script.js 	new file:   styles.css

---
+++ script.js
@@ -0,0 +1,37 @@
+const sendButton = document.getElementById("botão-enviar");
+const textInput = document.getElementById("input-text");
+const previewLista = document.getElementById("molde-lista");
+let itensArray = [];
+
+if (sendButton && textInput && previewLista && itensArray != null) {
+console.log("As constantes funcionam adequadamente.")
+} else {
+    console.log("Reveja as constantes e revise o código.")
+};
+
+//Eventos de click
+
+
+sendButton.addEventListener('click', () => {
+    const item = document.createElement("li");
+    const deleteButton = document.createElement("button");
+    const textItem = document.createElement("span")
+
+    textItem.classList.add("texto-item");
+    item.classList.add("item-lista");
+    deleteButton.classList.add("botao-excluir");
+    textItem.textContent = textInput.value;
+
+    previewLista.appendChild(item);
+
+    itensArray.push(textInput.value);
+    deleteButton.textContent = "X";
+    item.appendChild(textItem);
+    item.appendChild(deleteButton);
+    console.log(item);
+
+    deleteButton.addEventListener('click', () => {
+        previewLista.removeChild(item)
+    })
+
+});
